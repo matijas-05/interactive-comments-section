6906e16321a5177a0ddec821da53fcacb803ae14
Mateusz Wojtyna <w_mateusz@hotmail.com>
2022-04-06 17:13:58 +0200

    refactor: changed transitions, added more props to BtnWithImg

--- src/components/Buttons/BtnWithIcon.tsx
+++ src/components/Buttons/BtnWithIcon.tsx
@@ -1,13 +1,15 @@
 import styles from "./BtnWithIcon.module.scss"
 
 interface BtnWithIconProps {
-	imgSrc: string,
-	text: string,
+	imgSrc?: string,
+	alt?: string,
+	text?: string,
+	onClick?: () => void,
 }
 function BtnWithIcon(props: BtnWithIconProps) {
 	return (
-		<div className={`${styles["btn-with-icon"]} `}>
-			<img src={props.imgSrc} alt="" />
+		<div className={styles["btn-with-icon"]} onClick={props.onClick}>
+			<img src={props.imgSrc} alt={props.alt} />
 			<p className="accent-purple">{props.text}</p>
 		</div>
 	);
--- src/components/Comment/Comment.tsx
+++ src/components/Comment/Comment.tsx
@@ -13,7 +13,7 @@ function Comment() {
 			<p>Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.</p>
 			<div className="left-right">
 				<Votes />
-				<BtnWithIcon imgSrc="/src/assets/images/icon-reply.svg" text="Reply" />
+				<BtnWithIcon imgSrc="/src/assets/images/icon-reply.svg" text="Reply" onClick={() => console.log("reply")} />
 			</div>
 		</div>
 	);
