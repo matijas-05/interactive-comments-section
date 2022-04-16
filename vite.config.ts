4612ed8db1577b1bc725978bfb888608f37f0a49
Mateusz Wojtyna <w_mateusz@hotmail.com>
2022-04-17 01:38:24 +0200

    chore: allow vite to share live version in local network

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: "/interactive-comments-section/",
 	plugins: [
 		react()
 	],
-	base: "/interactive-comments-section/"
+	server: {
+		host: true
+	}
 });
