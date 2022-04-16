0e3b55c5678d33fb8809b04336810d566a624dc4
Mateusz Wojtyna <w_mateusz@hotmail.com>
2022-04-16 23:53:43 +0200

    feat: add width and height attrib to img

--- src/components/General/ButtonSecondary.tsx
+++ src/components/General/ButtonSecondary.tsx
@@ -9,9 +9,12 @@ interface Props {
 	children?: string | React.ReactNode,
 }
 function ButtonSecondary(props: Props) {
+	const img = new Image();
+	img.src = props.iconSrc ?? "";
+
 	return (
 		<button className={`${styles["btn-secondary"]} ${!props.noHoverEffect && "hover-opacity"} ${props.className}`} onClick={props.onClick}>
-			<img src={props.iconSrc} alt={typeof props.children === "string" ? props.children : props.alt} />
+			<img src={props.iconSrc} width={img.width} height={img.height} alt={typeof props.children === "string" ? props.children : props.alt} />
 			{props.children}
 		</button>
 	);
