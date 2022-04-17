de565addcf042601a27a60fc3437a16b37cedbdb
Mateusz Wojtyna <w_mateusz@hotmail.com>
2022-04-17 03:00:00 +0200

    feat: scrollMargin

--- src/components/Auth/Header.tsx
+++ src/components/Auth/Header.tsx
@@ -4,12 +4,22 @@ import styles from "./Header.module.scss";
 
 import logInIcon from "/src/assets/images/icon-log-in.svg";
 import signUpIcon from "/src/assets/images/icon-sign-up.svg";
+import { useEffect, useRef } from "react";
 
 function Header() {
 	const btnTextBreakpoint = 650;
+	const headerRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		const setScrollMargin = () => {
+			document.querySelectorAll<HTMLElement>("*").forEach(el => el.style.scrollMargin = `${headerRef.current!.offsetHeight + 10}px`);
+		};
+		
+		setInterval(() => setScrollMargin(), 100);
+	}, []);
 
 	return (
-		<header className={`${styles["header"]} left-right g-1 card`}>
+		<header ref={headerRef} className={`${styles["header"]} left-right g-1 card`}>
 			<h2 className={`${styles["header-text"]}`}>Interactive comments section</h2>
 			<nav className={`${styles["navbar"]} f-row g-1`}>
 				<MediaQuery maxWidth={btnTextBreakpoint - 1}><hr /></MediaQuery>
