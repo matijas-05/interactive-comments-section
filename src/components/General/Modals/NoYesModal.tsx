5dcac97a80e4ae54350722441c2aaf946e4fb149
Mateusz Wojtyna <w_mateusz@hotmail.com>
2022-04-19 16:57:47 +0200

    refactor: abstract away modal's popup functionality into 'PopupModal'

--- src/components/General/Modals/NoYesModal.tsx
+++ src/components/General/Modals/NoYesModal.tsx
@@ -1,7 +1,5 @@
-import ReactModal from "react-modal";
 import ButtonPrimary from "../Buttons/ButtonPrimary";
-import { disableBodyScroll, enableBodyScroll } from "body-scroll-lock";
-import styles from "./NoYesModal.module.scss";
+import PopupModal from "./PopupModal";
 
 interface Props {
 	header: string,
@@ -12,25 +10,10 @@ interface Props {
 }
 function NoYesModal(props: Props) {
 	return (
-		<ReactModal
-			className={{
-				base: `${styles["NoYesModal__Content"]} f-col g-1 card`,
-				afterOpen: styles["NoYesModal__Content--after-open"],
-				beforeClose: styles["NoYesModal__Content--before-close"]
-			}}
-			bodyOpenClassName="NoYesModal__Body"
-			portalClassName="NoYesModalPortal"
-			overlayClassName={{
-				base: `${styles["NoYesModal__Overlay"]} f-center`,
-				afterOpen: styles["NoYesModal__Overlay--after-open"],
-				beforeClose: styles["NoYesModal__Overlay--before-close"]
-			}}
-
-			contentLabel="Delete comment" ariaHideApp={false}
-			isOpen={props.modalState} shouldCloseOnEsc={true} shouldCloseOnOverlayClick={true}
-			closeTimeoutMS={200} onRequestClose={props.cancelModal}
-			onAfterOpen={() => disableBodyScroll(document.querySelector("body")!)}
-			onAfterClose={() => enableBodyScroll(document.querySelector("body")!)}
+		<PopupModal
+			contentLabel="Delete comment"
+			isOpen={props.modalState}
+			onRequestClose={props.cancelModal}
 		>
 			<h1>{props.header}</h1>
 			<p>{props.message}</p>
@@ -38,7 +21,7 @@ function NoYesModal(props: Props) {
 				<ButtonPrimary className="f-1" onClick={props.cancelModal}>NO, CANCEL</ButtonPrimary>
 				<ButtonPrimary className="f-1 bg-red" onClick={props.confirmModal}>YES, DELETE</ButtonPrimary>
 			</div>
-		</ReactModal>
+		</PopupModal>
 	);
 }
 
---
+++ src/components/General/Modals/PopupModal.tsx
@@ -0,0 +1,37 @@
+import ReactModal from "react-modal";
+import { disableBodyScroll, enableBodyScroll } from "body-scroll-lock";
+import styles from "./PopupModal.module.scss";
+
+interface Props extends ReactModal.Props {
+	onRequestClose: () => void;
+}
+function PopupModal(props: Props) {
+	return (
+		<ReactModal
+			{...props}
+
+			className={{
+				base: `${styles["PopupModal__Content"]} f-col g-1 card`,
+				afterOpen: styles["PopupModal__Content--after-open"],
+				beforeClose: styles["PopupModal__Content--before-close"]
+			}}
+			bodyOpenClassName="PopupModal__Body"
+			portalClassName="PopupModalPortal"
+			overlayClassName={{
+				base: `${styles["PopupModal__Overlay"]} f-center`,
+				afterOpen: styles["PopupModal__Overlay--after-open"],
+				beforeClose: styles["PopupModal__Overlay--before-close"]
+			}}
+
+			shouldCloseOnEsc={true} shouldCloseOnOverlayClick={true}
+			closeTimeoutMS={200} onRequestClose={props.onRequestClose}
+			onAfterOpen={() => disableBodyScroll(document.querySelector("body")!)}
+			onAfterClose={() => enableBodyScroll(document.querySelector("body")!)}
+			ariaHideApp={false}
+		>
+			{props.children}
+		</ReactModal>
+	);
+}
+
+export default PopupModal;
