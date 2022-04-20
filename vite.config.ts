65a788e9d66c5be26f1d3737004d3bab01255a60
Mateusz Wojtyna <w_mateusz@hotmail.com>
2022-04-20 17:55:30 +0200

    chore: open firefox when starting vite server

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath, URL } from "url";
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore: no need to import @types/node
+process.env.BROWSER = "firefox";
+
 // https://vitejs.dev/config/
 export default defineConfig({
 	base: "/interactive-comments-section/",
@@ -9,7 +13,8 @@ export default defineConfig({
 		react()
 	],
 	server: {
-		host: true
+		host: true,
+		open: true
 	},
 	resolve: {
 		alias: {
