3af13ebb60016372c2449ba299aac7ae11f8a5b1
Mateusz Wojtyna <w_mateusz@hotmail.com>
2022-04-20 19:09:36 +0200

    feat: add normalize.css

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import App from "./components/App";
 import ReactDOM from "react-dom";
 import { CurrentUser } from "./context";
+
+import "normalize.css";
 import "./scss/index.scss";
 
 ReactDOM.render(
