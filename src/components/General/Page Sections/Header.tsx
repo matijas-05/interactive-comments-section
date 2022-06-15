0bc90d766d22b0e8812c5898419a07e00955d717
3IB1 <w_mateusz@hotmail.com>
2022-06-15 08:49:10 +0200

    feat (Header): remove 'Sign up' btn when signed in

--- src/components/General/Page Sections/Header.tsx
+++ src/components/General/Page Sections/Header.tsx
@@ -29,33 +29,39 @@ function Header(props: Props) {
 			<nav className={`${styles["navbar"]} f-row g-1`}>
 				<MediaQuery maxWidth={btnTextBreakpoint - 1}><hr className="vertical-separator" /></MediaQuery>
 
-				{getCurrentUser() ?
-					<ButtonSecondary
-						className="text-purple" iconClassName={`${styles["icon"]} fa-lg`}
-						faIcon={faArrowRightFromBracket}
-						alt="Sign in" onClick={props.openSignOutModal}
-					>
-						<MediaQuery minWidth={btnTextBreakpoint}>Sign out</MediaQuery>
-					</ButtonSecondary>
-					:
-					<ButtonSecondary
-						className="text-purple" iconClassName={`${styles["icon"]} fa-lg`}
-						faIcon={faArrowRightToBracket}
-						alt="Sign in" onClick={props.openSignInModal}
-					>
-						<MediaQuery minWidth={btnTextBreakpoint}>Sign in</MediaQuery>
-					</ButtonSecondary>
+				{
+					getCurrentUser() ?
+						<ButtonSecondary
+							className="text-purple" iconClassName={`${styles["icon"]} fa-lg`}
+							faIcon={faArrowRightFromBracket}
+							alt="Sign in" onClick={props.openSignOutModal}
+						>
+							<MediaQuery minWidth={btnTextBreakpoint}>Sign out</MediaQuery>
+						</ButtonSecondary>
+						:
+						<ButtonSecondary
+							className="text-purple" iconClassName={`${styles["icon"]} fa-lg`}
+							faIcon={faArrowRightToBracket}
+							alt="Sign in" onClick={props.openSignInModal}
+						>
+							<MediaQuery minWidth={btnTextBreakpoint}>Sign in</MediaQuery>
+						</ButtonSecondary>
 				}
 
-				<MediaQuery minWidth={btnTextBreakpoint}><hr className="vertical-separator" /></MediaQuery>
 
-				<ButtonSecondary
-					className="text-purple" iconClassName={`${styles["icon"]} fa-lg`}
-					faIcon={faUserPlus}
-					alt="Sign up" onClick={props.openSignUpModal}
-				>
-					<MediaQuery minWidth={btnTextBreakpoint}>Sign up</MediaQuery>
-				</ButtonSecondary>
+				{
+					!getCurrentUser() &&
+					<>
+						<MediaQuery minWidth={btnTextBreakpoint}><hr className="vertical-separator" /></MediaQuery>
+						<ButtonSecondary
+							className="text-purple" iconClassName={`${styles["icon"]} fa-lg`}
+							faIcon={faUserPlus}
+							alt="Sign up" onClick={props.openSignUpModal}
+						>
+							<MediaQuery minWidth={btnTextBreakpoint}>Sign up</MediaQuery>
+						</ButtonSecondary>
+					</>
+				}
 			</nav>
 		</header >
 	);
