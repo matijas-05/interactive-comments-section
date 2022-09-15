cac69f1dee61bb502c31a02d5a7bee2777520511
Mateusz Wojtyna <w_mateusz@hotmail.com>
2022-09-15 02:19:07 +0200

    chore: update to react 18

--- src/components/General/MediaQueryComponents.tsx
+++ src/components/General/MediaQueryComponents.tsx
@@ -1,13 +1,16 @@
-import { PropsWithChildren, ReactNode } from "react";
+import { ReactNode } from "react";
 import MediaQuery from "react-responsive";
 
 const breakpoint = 768;
-export const Mobile = (props: PropsWithChildren<ReactNode>) => {
+interface Props {
+	children?: ReactNode
+}
+export const Mobile = (props: Props) => {
 	return (
 		<MediaQuery maxWidth={breakpoint}>{props.children}</MediaQuery>
 	);
 };
-export const Desktop = (props: PropsWithChildren<ReactNode>) => {
+export const Desktop = (props: Props) => {
 	return (
 		<MediaQuery minWidth={breakpoint + 1}>{props.children}</MediaQuery>
 	);
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import App from "./components/App";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 
 import "normalize.css";
 import "./scss/index.scss";
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root")!);
+root.render(
 	<React.StrictMode>
 		<App />
-	</React.StrictMode>,
-
-	document.getElementById("root")
+	</React.StrictMode>
 );
